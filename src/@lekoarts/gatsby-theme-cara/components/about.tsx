3cf820d4c4f23a2bf7dae9cc280a311a10334535
Mark Edmonds <mark@mjedmonds.com>
2020-05-02 16:13:15 -0500

    initial work on particle.js for cara website

--- src/@lekoarts/gatsby-theme-cara/components/about.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/about.tsx
@@ -7,14 +7,36 @@ import SVG from "./svg"
 import { UpDown, UpDownWide } from "../styles/animations"
 // @ts-ignore
 import AboutMDX from "../sections/about"
+import Particles from 'react-particles-js'
 
 const About = ({ offset }: { offset: number }) => (
   <div>
     <Divider bg="divider" clipPath="polygon(0 16%, 100% 4%, 100% 82%, 0 94%)" speed={0.2} offset={offset} />
     <Content speed={0.4} offset={offset}>
-      <Inner>
-        <AboutMDX />
-      </Inner>
+      <Particles params={
+        {
+          "particles": {
+              "number": {
+                  "value": 50
+              },
+              "size": {
+                  "value": 3
+              }
+          },
+          "interactivity": {
+              "events": {
+                  "onhover": {
+                      "enable": true,
+                      "mode": "repulse"
+                  }
+              }
+            }
+          }
+      } id="particles-js">
+        <Inner>
+          <AboutMDX />
+        </Inner>
+      </Particles>
     </Content>
   </div>
 )
--- src/@lekoarts/gatsby-theme-cara/components/paypal.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/paypal.tsx
@@ -9,7 +9,7 @@ const Paypal = () => (
       <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <input type="hidden" name="cmd" value="_s-xclick" />
         <input type="hidden" name="hosted_button_id" value="BCYPHVW52TNFW" />
-        <input type="image" src="/paypal.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+        <input type="image" src="/cara.ai/paypal.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
         <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
       </form>
     </div>
