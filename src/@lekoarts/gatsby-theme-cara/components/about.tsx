7d88017e4a505db0ac9bd395d984a92d697fe7fe
Mark Edmonds <mark@mjedmonds.com>
2020-05-03 10:43:57 -0500

    Moved particles to hero

--- src/@lekoarts/gatsby-theme-cara/components/about.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/about.tsx
@@ -7,45 +7,11 @@ import SVG from "./svg"
 import { UpDown, UpDownWide } from "../styles/animations"
 // @ts-ignore
 import AboutMDX from "../sections/about"
-import Particles from 'react-particles-js'
 
 const About = ({ offset }: { offset: number }) => (
   <div>
     <Divider bg="divider" clipPath="polygon(0 16%, 100% 4%, 100% 82%, 0 94%)" speed={0.2} offset={offset} />
     <Content speed={0.4} offset={offset}>
-      <Particles
-          className="particle-js"
-          height={"65%"}
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            top: "150px",
-            left: "-0%",
-          }}
-          params={{
-            particles: {
-              color: {
-                value: "#2b8a9d"
-              },
-              size: {
-                value: "2"
-              },
-              number: {
-                value: "150"
-              },
-              line_linked: {
-                enable: {
-                  auto: true
-                },
-                distance: "150",
-                color: "#2b8a9d",
-                opacity: "0.3",
-                width: "1"
-              }
-            },
-          }}
-        />
         <Inner>
           <AboutMDX />
         </Inner>
--- src/@lekoarts/gatsby-theme-cara/components/hero.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/hero.tsx
@@ -6,10 +6,44 @@ import Content from "../elements/content"
 // @ts-ignore
 import Intro from "../sections/intro"
 import Title from "../sections/title"
+import Particles from 'react-particles-js'
 
 const Hero = ({ offset }: { offset: number }) => (
   <div>
     <Content speed={0.4} offset={offset}>
+      <Particles
+          className="particle-js"
+          height={"100%"}
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            top: "150px",
+            left: "-0%",
+          }}
+          params={{
+            particles: {
+              color: {
+                value: "#2b8a9d"
+              },
+              size: {
+                value: "2"
+              },
+              number: {
+                value: "150"
+              },
+              line_linked: {
+                enable: {
+                  auto: true
+                },
+                distance: "150",
+                color: "#2b8a9d",
+                opacity: "0.3",
+                width: "1"
+              }
+            },
+          }}
+        />
       <Inner>
         <Title />
       </Inner>
